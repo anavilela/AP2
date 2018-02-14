e04fc137e9ed0aae6799fca4ef1d9c7e0ff7d326
anavilela <apfvboaventura@gmail.com>
2018-02-14 10:18:06 -0200

    Resolução da prova de AP2

---
+++ prova/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+//void ConvertToBinary(int n);
+int main() {
+    ConvertToBinary(5);
+    return 0;
+}
+void ConvertToBinary(int n)
+{
+    if (n / 2 != 0) {
+        ConvertToBinary(n / 2);
+    }
+    printf("%d", n % 2);
+}
