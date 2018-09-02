3bb3a387f917e9b06608af4afee5f24c78fc68b5
anavilela <apfvboaventura@gmail.com>
2018-09-02 19:51:01 -0300

    Add files via upload

---
+++ passagem de parametros/calculadora.h
@@ -0,0 +1,22 @@
+int soma(int i, int j)
+{
+    return i+j;
+}
+
+int subt(int i, int j)
+{
+    return i-j;
+}
+
+int mult(int i, int j)
+{
+    return i*j;
+}
+
+float divi(int i, int j)
+{
+    float x,y;
+    x=i;
+    y=j;
+    return x/y;
+}
